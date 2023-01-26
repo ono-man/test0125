fedd713ee67ca79ef643bd46dba066fc9868e266
クロハク <48093735+kurohakusui@users.noreply.github.com>
2023-01-26 12:08:30 +0900

    修正
    惜しかったです

--- test0125_ono(form)/git-test_ono.sql
+++ test0125_ono(form)/git-test_ono.sql
@@ -1,7 +1,12 @@
-DELETE DATABASE Git-test;
+-- DELETE DATABASE Git-test;
+--既にデータベースがない状態の人を想定しているので
+--その方がインポートするとエラーが出ます。
+--DELETEはデータを消すもので、database消すものではないです、正しくはDROP
 
-CREATE DATABASE Git-test;
-USE Git-test;
+CREATE DATABASE `Git-test`;
+USE `Git-test`;
+
+--エスケープがなかったのでハイフンがでエラーが出ました
 
 CREATE TABLE `comments` (
   `no` int(11) NOT NULL,
@@ -9,8 +14,9 @@ CREATE TABLE `comments` (
   `address` varchar(30) NOT NULL,
   `message` varchar(400) NOT NULL,
   `created` datetime NOT NULL
-);
-
+)
+--;入らないです。
 DEFAULT CHARSET=utf8;
 
-INSERT INTO `comments` (`no`, `name`, `address`, `message`, `created`) VALUES
+-- INSERT INTO `comments` (`no`, `name`, `address`, `message`, `created`) VALUES
+--何かデータ登録しましたか？
